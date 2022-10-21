f9a9574e92e54aacd5deabfbd356ff98af6492c0
Muhammad Ali <48137657+alitheDev@users.noreply.github.com>
2022-10-21 16:28:17 +0500

    Sum of 2 Numbers

---
+++ ToddlersLevel/SumOf2Numbers.cpp
@@ -0,0 +1,16 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a=12;
+int b=12;
+int c = a+b;
+ cout<<"the answer is "<<c<<endl;
+
+    return 0;
+
+
+}
